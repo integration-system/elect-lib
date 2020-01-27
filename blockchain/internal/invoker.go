392aaba845a65efa6fc099d52c99e56074060499
DeniskaRediska <grj.den@gmail.com>
2020-01-27 16:16:03 +0300

    ref

--- blockchain/internal/invoker.go
+++ blockchain/internal/invoker.go
@@ -13,7 +13,7 @@ type Client struct {
 	headers          map[string]string
 	completeInitAuth bool
 	cli              *fasthttp.HostClient
-	mx               sync.Mutex
+	mx               sync.RWMutex
 }
 
 func NewClient(address, login, password string) *Client {
@@ -27,16 +27,29 @@ func NewClient(address, login, password string) *Client {
 }
 
 func (b *Client) Invoke(url string, request []byte, responsePtr interface{}) error {
+	b.mx.RLock()
+	completeInitAuth := b.completeInitAuth
+	b.mx.RUnlock()
+	if !completeInitAuth {
+		err := b.auth()
+		if err != nil {
+			return err
+		}
+	}
+
 	response := Response{Result: responsePtr}
-	statusCode, err := b.invokeWithConvertResponse(url, request, &response)
+	statusCode, err := b.invoke(url, request, &response)
 	if err != nil {
 		return err
 	}
 	if response.Error != nil {
 		if statusCode == fasthttp.StatusUnauthorized {
+			b.mx.Lock()
+			b.completeInitAuth = false
+			b.mx.Unlock()
 			err := b.auth()
 			if err == nil {
-				_, err = b.invokeWithConvertResponse(url, request, &response)
+				_, err = b.invoke(url, request, &response)
 				if err != nil {
 					return err
 				}
@@ -51,25 +64,7 @@ func (b *Client) Invoke(url string, request []byte, responsePtr interface{}) err
 	return nil
 }
 
-func (b *Client) invokeWithConvertResponse(url string, request []byte, respPtr interface{}) (int, error) {
-	response, statusCode, err := b.invoke(url, request)
-	if err != nil {
-		return 0, err
-	}
-	err = json.Unmarshal(response, respPtr)
-	if err != nil {
-		return 0, err
-	}
-	return statusCode, nil
-}
-
-func (b *Client) invoke(uri string, request []byte) ([]byte, int, error) {
-	if !b.completeInitAuth {
-		err := b.auth()
-		if err != nil {
-			return nil, 0, err
-		}
-	}
+func (b *Client) invoke(uri string, request []byte, respPtr interface{}) (int, error) {
 	req, res := fasthttp.AcquireRequest(), fasthttp.AcquireResponse()
 	defer fasthttp.ReleaseRequest(req)
 	defer fasthttp.ReleaseResponse(res)
@@ -87,18 +82,25 @@ func (b *Client) invoke(uri string, request []byte) ([]byte, int, error) {
 
 	err := b.cli.DoTimeout(req, res, time.Second*15)
 	if err != nil {
-		return nil, 0, err
+		return 0, err
 	}
 
 	statusCode := res.StatusCode()
-	response := res.Body()
-	return response, statusCode, nil
+	err = json.Unmarshal(res.Body(), respPtr)
+	if err != nil {
+		return 0, err
+	}
+	return statusCode, nil
 }
 
 func (b *Client) auth() error {
 	b.mx.Lock()
+	defer b.mx.Unlock()
+
+	if b.completeInitAuth {
+		return nil
+	}
 	b.completeInitAuth = true
-	b.mx.Unlock()
 	request := authenticateRequest{
 		Login:    b.login,
 		Password: b.password,
@@ -109,15 +111,14 @@ func (b *Client) auth() error {
 	}
 	result := authenticateResponse{}
 	response := Response{Result: &result}
-	_, err = b.invokeWithConvertResponse(authenticateMethod, req, &response)
+	_, err = b.invoke(authenticateMethod, req, &response)
 	if err != nil {
 		return err
 	}
 	if response.Error != nil {
 		return response.ConvertError()
 	}
-	b.mx.Lock()
+
 	b.headers["Authorization"] = "Bearer " + result.Token
-	b.mx.Unlock()
 	return nil
 }
