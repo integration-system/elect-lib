e00eb8431eaf159c7d4c0915d3d9df2237be421c
Maxim Merzhanov <maksimmerzh@gmail.com>
2020-02-18 12:37:29 +0300

    add new methods to election service interface

--- service/election/interface.go
+++ service/election/interface.go
@@ -3,5 +3,7 @@ package election
 type Service interface {
 	GetCurrentRegElectionsByIdList(IdentityListRequest) ([]Election, error)
 	GetCurrentVotingElectionsByIdList(IdentityListRequest) ([]Election, error)
+	GetRegElectionById(IdentityRequest) (*Election, error)
+	GetVotingElectionById(IdentityRequest) (*Election, error)
 	GetElectionById(IdentityRequest) (*Election, error)
 }
