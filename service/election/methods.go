0249009e6727c286ba690915f1d4013a5a0a0d82
DeniskaRediska <grj.den@gmail.com>
2020-03-03 15:03:22 +0300

    update path for 'get_election_by_id'

--- service/election/methods.go
+++ service/election/methods.go
@@ -1,9 +1,9 @@
 package election
 
 const (
+	getElectionsById                  = "elect-election/operator/get_by_id"
 	getCurrentRegElectionsByIdList    = "elect-election/internal/get_reg_elections"
 	getCurrentVotingElectionsByIdList = "elect-election/internal/get_voting_elections"
-	getElectionsById                  = "elect-election/internal/get_by_id"
 	getRegElectionById                = "elect-election/internal/get_reg_election_by_id"
 	getVotingElectionById             = "elect-election/internal/get_voting_election_by_id"
 )
